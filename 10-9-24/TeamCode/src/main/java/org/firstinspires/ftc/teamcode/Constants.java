ac8688faf9e7dec350aece2b35e82b0677e288c9
BobThe10345th <Flattopchopshop@gmail.com>
2024-11-27 18:36:14 -0600

    added relevant values

--- 10-9-24/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ 10-9-24/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -9,9 +9,29 @@ public class Constants
         Blue
     }
 
+     public final static int HIGH_BUCKET = 5440 + 75;
+    public final static int LOW_BUCKET = 2780;
+    public final static int LOW_SAMPLE = 980;
+    public final static int HIGH_SAMPLE = 2640;
+
+    public final static int R =  ;
+    public final static int F_B = frontRight + frontLeft / 2;
+    public final static int L_R = back;
+    
+    public final static int sub1 = 0;
+    public final static int sub2 = 0;
+    public final static int sample1 = 0;
+    public final static int sample2 = 0;
+    public final static int RR = 0 ;
+    public final static int LR = 0 ;  
+    
+
+    
+
+    
 
 
 
-    public static Team CURRENT_TEAM = Team.Undetermined;
+    
 
 }
