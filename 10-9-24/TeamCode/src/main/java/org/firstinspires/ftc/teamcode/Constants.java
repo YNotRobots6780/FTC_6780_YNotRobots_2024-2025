0366554a9b30bd803d3d4dff55e3ac54bdb239dd
BobThe10345th <Flattopchopshop@gmail.com>
2024-12-01 15:40:34 -0600

    added path finding values

--- 10-9-24/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ 10-9-24/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -1,5 +1,3 @@
-package org.firstinspires.ftc.teamcode;
-
 public class Constants
 {
     public enum Team
@@ -14,24 +12,20 @@ public class Constants
     public final static int LOW_SAMPLE = 980;
     public final static int HIGH_SAMPLE = 2640;
 
-    public final static int R =  ;
+    
     public final static int F_B = frontRight + frontLeft / 2;
     public final static int L_R = back;
     
-    public final static int sub1 = 0;
-    public final static int sub2 = 0;
-    public final static int sample1 = 0;
-    public final static int sample2 = 0;
+    public final static int sub1 = 0,0;
+    public final static int sub2 = 0,0;
+    public final static int sample1 = 0,0;
+    public final static int sample2 = 0,0;
     public final static int RR = 0 ;
     public final static int LR = 0 ;  
-    
-
-    
-
-    
-
-
-
-    
+    public final static int currentPoshion = F_B,L_R;
+    public final static int sa1 = 0;
+    public final static int sa2 = 0;
+    public final static int su1 = 0;
+    public final static int su2 = 0; 
 
 }
